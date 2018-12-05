8b254661cf2d695f7742e7d5143bd883897b11a9
mrskeep <45632861+mrskeep@users.noreply.github.com>
2018-12-05 16:28:45 -0200

    Create 1006.c

---
+++ -include -stdio.h-   -include -stdlib.h-    int main-     double A- B - C- SOMA-      scanf-lf-lf-lf-A-B-C-      SOMA - -A-2 - B-3 - C-5/1006.c
@@ -0,0 +1,12 @@
+  #include <stdio.h>
+  #include <stdlib.h>
+
+  int main(){
+    double A, B , C, SOMA;
+
+    scanf("%lf%lf%lf",&A,&B,&C);
+
+    SOMA = (A*2 + B*3 + C*5)/10;
+
+    printf("MEDIA = %0.1lf\n",SOMA);
+  }
