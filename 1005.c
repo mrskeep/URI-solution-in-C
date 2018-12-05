beb43c477f092907784918be9bab1b6b7dfd022b
mrskeep <45632861+mrskeep@users.noreply.github.com>
2018-12-05 16:25:50 -0200

    Create 1005.c

---
+++ 1005.c
@@ -0,0 +1,12 @@
+  #include <stdio.h>
+  #include <stdlib.h>
+
+  int main(){
+    double A, B , SOMA;
+
+    scanf("%lf%lf",&A,&B);
+
+    SOMA = (A*3.5 + B*7.5)/11;
+
+    printf("MEDIA = %0.5lf\n",SOMA);
+  }
