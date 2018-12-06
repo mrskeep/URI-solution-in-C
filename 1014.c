e298f1796b4ff849651696098ba78eac7e2b5ba5
mrskeep <45632861+mrskeep@users.noreply.github.com>
2018-12-05 22:11:13 -0200

    Create 1014.c

---
+++ 1014.c
@@ -0,0 +1,15 @@
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  #include <math.h>
+
+  int main(){
+    int a;
+    float b;
+
+    scanf("%d%f",&a,&b);
+
+    b = a/b;
+    printf("%.3f km/l\n",b);
+
+  }
